20c39f1a1a8abda0c16dae33e14b719f5ded4e1c
sulman <msulmmehmood@gmail.com>
2025-03-06 17:32:55 -0800

    making welcome page

--- src/dashbroadpage.jsx
+++ src/dashbroadpage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const DashBroadPage =()=>{
     return(
-        <div className="container-fluid">
+        <div className="container-fluid bg-light">
         <div className="row">
             <div className="col-6">
                 <h1 className="text-primary" style={{
@@ -12,10 +12,35 @@ const DashBroadPage =()=>{
                 with the people in your life.</p> 
             </div>
             <div className="col-6">
-                <form action="">
-                    <input type="text" className="" />
-                    <input type="text" />
-                </form>
+            <div className="d-flex justify-content-center align-items-center vh-100 ">
+      <div className="p-4 shadow bg-white rounded" style={{ width: "400px" }}>
+        <input
+          type="text"
+          className="form-control mb-3"
+          placeholder="Email address or phone number"
+        />
+        <input
+          type="password"
+          className="form-control mb-3"
+          placeholder="Password"
+        />
+        <button className="btn btn-primary w-100 mb-3">Log in</button>
+        <div className="text-center">
+          <a href="#" className="text-primary">
+            Forgotten password?
+          </a>
+        </div>
+        <hr />
+        <div className="text-center">
+          <button className="btn btn-success">Create new account</button>
+        </div>
+      </div>
+      <div className="position-absolute bottom-0 text-center w-100 p-3">
+        <p>
+          <strong>Create a Page</strong> for a celebrity, brand or business.
+        </p>
+      </div>
+    </div>
             </div>
             </div>        
         </div>
